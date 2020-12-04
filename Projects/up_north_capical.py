1b7d930be11aa906100a5e7065350f4b19fdac76
VoidFly <502576738@qq.com>
2020-12-04 16:03:39 +0800

    1204

--- Projects/up_north_capical.py
+++ Projects/up_north_capical.py
@@ -9,7 +9,7 @@ import pandas as pd
 import pymssql
 conn = pymssql.connect(host='192.168.0.144', user='readonly', password='readonly', database='jydb')
 
-factor_path=r'C:\Projects\up_north_capital\data\HoldPER_N.csv'
+factor_path=r'C:\Projects\up_north_capital\data\Hold.csv'
 pricing_path=r'C:\Projects\up_north_capital\data\Pricing.csv'
 
 #对于my_factor, 需要先指定dtype={'asset':str}, 并且不能设置index_col,之后再set_index(否则asset会读成int)
@@ -44,16 +44,16 @@ market_data=market_data.shift(-1)
 # Ingest and format data
 factor_data = alphalens.utils.get_clean_factor_and_forward_returns(my_factor,
                                                                    pricing,
-                                                                   periods=[1],
-                                                                   quantiles=10,
+                                                                   periods=[1,5,20],
+                                                                   quantiles=5,
                                                                    groupby=None,
                                                                    groupby_labels=None
+                                                                   
                                                                    )
-#market_data1=alphalens.utils.compute_market_index_forward_returns(my_factor,market_data,periods=[1,5])
+market_data1=alphalens.utils.compute_market_index_forward_returns(my_factor,market_data,periods=[1,5,20])
 # Run analysis
-alphalens.tears.create_full_tear_sheet(factor_data,long_short=False)
+alphalens.tears.create_full_tear_sheet(factor_data,index_name=market_index_name,market_index=market_data1,long_short=False)
 
 #alphalens.tears.create_information_tear_sheet(factor_data)
 # %%
 
-# %%
